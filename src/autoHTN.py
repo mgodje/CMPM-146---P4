e224efbc24a06dead2fba1e44aedbdff5b0520f2
AndrBy1 <150852574+AndrBy1@users.noreply.github.com>
2024-07-26 00:19:02 -0700

    overhauled method and operator, method has issues

--- src/autoHTN.py
+++ src/autoHTN.py
@@ -1,5 +1,6 @@
 import pyhop
 import json
+import types
 
 def check_enough (state, ID, item, num):
 	if getattr(state,item)[ID] >= num: return []
@@ -18,18 +19,19 @@ pyhop.declare_methods ('produce', produce)
 def make_method (name, rule):
 	def method (state, ID):
 		# your code here
+		#print(rule)
+		#print(rule[1]['Requires'])
 		arr = []
-		for item in rule["Requires"]:
+		for item in rule[1]['Requires']:
 			enough = ('have_enough', ID, item[0], item[1])
 			arr.append(enough)
-		for item in rule["Consumes"]:
+		for item in rule[1]['Consumes']:
 			enough = ('have_enough', ID, item[0], item[1])
 			arr.append(enough)
-
 		arr.append(name, ID)
 		return arr
 	
-	return method
+	return method(state, "agent")
 
 def declare_methods (data):
 	# some recipes are faster than others for the same product even though they might require extra tools
@@ -37,6 +39,185 @@ def declare_methods (data):
 
 	# your code here
 	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)	
+	for item in data['Items']:
+		methods = []
+		name = "produce_" + item
+		print("item: " + item)
+		for rule in data['Recipes'].items():
+			if item == list(rule[1]['Produces'])[0]:
+				print("true!!")
+				method = make_method
+				method.__name__ = rule[0].replace(" ", "_")
+				print("method name: " + method.__name__)
+				methods.append(method) 
+		print("name: " + name)
+		pyhop.declare_methods(name, *methods) 
+		
+	for item in data['Tools']:
+		methods = []
+		name = "produce_" + item
+		print("item: " + item)
+		for rule in data['Recipes'].items():
+			if item == list(rule[1]['Produces'])[0]:
+				print("true!!")
+				method = make_method
+				method.__name__ = rule[0].replace(" ", "_")
+				print("method name: " + method.__name__)
+				methods.append(method) 
+		print("name: " + name)
+		pyhop.declare_methods(name, *methods) 
+
+	
+	print("========methods=======")
+	pyhop.print_methods()
+
+
+def make_operator (rule):
+	def operator (state, ID):
+		# your code here
+		print(rule[0])
+		if rule[1]['Time'] < state.time[ID]:
+			#print(rule[1]['Time'])
+			return False
+		for item in rule[1]['Requires']:
+			#print("item: " + item)
+			print("getattr(state, item)[ID]" + getattr(state, item)[ID])
+			if getattr(state, item)[ID] < rule[1]['Requires'].get(item):
+				return False
+		for item in rule[1]['Consumes']:
+			if getattr(state, item)[ID] < rule[1]['Consumes'].get(item):
+				return False
+			else:
+				setattr(state, item, {ID: getattr(state, item)[ID] - rule[1]['Consumes'].get(item)})
+		for item in rule[1]['Produces']:
+			setattr(state, item[ID], rule[1]['Produces'].get(item))
+		
+		print("state: " + state)
+		return state
+	
+	return operator(state, "agent")
+
+def declare_operators (data):
+	# your code here
+	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
+	
+	for rule in data['Recipes'].items():
+		#print(rule)
+		operator = make_operator
+		operator.__name__ = "op_" + rule[0].replace(" ", "_")
+		print(operator)
+		pyhop.declare_operators(operator)
+	
+	print("========operators=======")
+	pyhop.print_operators()
+	
+	pass
+
+def add_heuristic (data, ID):
+	# prune search branch if heuristic() returns True
+	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
+	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
+	def heuristic (state, curr_task, tasks, plan, depth, calling_stack):
+		# your code here
+
+		# use the data to get the recipes and the tools
+		# use the ID to get the agent's ID
+
+		#state: the current problem state
+		#curr_task: the task addressed by every method in methods
+		#tasks: the list of tasks that follow curr_task in its sequential task list
+		#plan: the currently accrued plan of operations
+		#depth: the depth of curr_task in pyhop’s search tree
+		#calling-stack: the list of subtasks connecting the overall task to curr_task
+
+		# if the current task is complete, return true
+		# if the current task is not complete, check if the task can be completed
+		# if the task can be completed, return true
+		# else return false
+		
+		for task in tasks:
+			if task[0] == curr_task:
+				return False
+			if curr_task == 'produce_furnace':
+				if getattr(state,'furnace')[ID] >= 1: return True
+			if curr_task == 'produce_bench':
+				if getattr(state,'bench')[ID] >= 1: return True
+			if curr_task == 'produce_iron_pickaxe':
+				if getattr(state,'iron_pickaxe')[ID] >= 1: return True
+			if curr_task == 'produce_iron_axe':
+				if getattr(state,'iron_axe')[ID] >= 1: return True
+			if curr_task == 'produce_stone_pickaxe':
+				if getattr(state,'stone_pickaxe')[ID] >= 1: return True
+			if curr_task == 'produce_stone_axe':
+				if getattr(state,'stone_axe')[ID] >= 1: return True
+			if curr_task == 'produce_wooden_pickaxe':
+				if getattr(state,'wooden_pickaxe')[ID] >= 1: return True
+			if curr_task == 'produce_wooden_axe':
+				if getattr(state,'wooden_axe')[ID] >= 1: return True
+			if curr_task == 'produce_wood':
+				if getattr(state,'cart')[ID] >= 1: return True
+			if curr_task == 'produce_stick':
+				if getattr(state,'cart')[ID] >= 1: return True
+			if curr_task == 'produce_cart':
+				if getattr(state,'cart')[ID] >= 1: return True
+			if curr_task == 'produce_cart':
+				if getattr(state,'cart')[ID] >= 1: return True
+			if curr_task == 'produce_cart':
+				if getattr(state,'cart')[ID] >= 1: return True
+			if curr_task == 'produce_rail':
+				if getattr(state,'rail')[ID] >= 20: return True
+
+		return False
+
+	pyhop.add_check(heuristic)
+
+def set_up_state (data, ID, time=0):
+	state = pyhop.State('state')
+	state.time = {ID: time}
+
+	for item in data['Items']:
+		setattr(state, item, {ID: 0})
+
+	for item in data['Tools']:
+		setattr(state, item, {ID: 0})
+
+	for item, num in data['Initial'].items():
+		setattr(state, item, {ID: num})
+
+	return state
+
+def set_up_goals (data, ID):
+	goals = []
+	for item, num in data['Goal'].items():
+		goals.append(('have_enough', ID, item, num))
+
+	return goals
+
+if __name__ == '__main__':
+	rules_filename = 'crafting.json'
+
+	with open(rules_filename) as f:
+		data = json.load(f)
+
+	state = set_up_state(data, 'agent', time=239) # allot time here
+	goals = set_up_goals(data, 'agent')
+	"""print("state: ")
+	pyhop.print_state(state)
+	print(state.cart)"""
+
+	declare_operators(data)
+	declare_methods(data)
+	add_heuristic(data, 'agent')
+
+	# pyhop.print_operators()
+	# pyhop.print_methods()
+
+	# Hint: verbose output can take a long time even if the solution is correct; 
+	# try verbose=1 if it is taking too long
+	pyhop.pyhop(state, goals, verbose=3)
+	# pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+
+	"""FOR METHOD
 	def punch_for_wood (state, ID):
 		return make_method('op_punch_for_wood', data["Recipes"]["punch for wood"])
 	
@@ -128,228 +309,4 @@ def declare_methods (data):
 	pyhop.declare_methods ('produce_wooden_pickaxe', craft_wooden_pickaxe_at_bench)
 	pyhop.declare_methods ('produce_rail', craft_rail_at_bench)
 	pyhop.declare_methods ('produce_cart', craft_cart_at_bench)
-	pyhop.declare_methods ('produce_plank', craft_plank)	
-	
-def make_operator (rule):
-	def operator (state, ID):
-		# your code here
-		if rule["Time"] < state.time[ID]:
-			return False
-		for item in rule["Requires"]:
-			if getattr(state, item)[ID] < rule["Requires"].get(item):
-				return False
-		for item in rule["Consumes"]:
-			if getattr(state, item)[ID] < rule["Consumes"].get(item):
-				return False
-			else:
-				setattr(state, item, {ID: getattr(state, item)[ID] - rule["Consumes"].get(item)})
-		for item in rule["Produces"]:
-			setattr(state, item[ID], rule["Produces"].get(item))
-		return state
-	
-	return operator
-
-def declare_operators (data):
-	# your code here
-	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	def op_punch_for_wood(state, ID):
-		state = make_operator(data["Recipes"]["punch for wood"])
-		return state
-	
-	def op_craft_wooden_axe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft wooden_axe at bench"])
-		return state
-	
-	def op_iron_axe_for_wood(state, ID):
-		state = make_operator(data["Recipes"]["iron_axe for wood"])
-		return state
-	
-	def op_craft_wooden_pickaxe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft wooden_pickaxe at bench"])
-		return state
-	
-	def op_craft_stone_pickaxe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft stone_pickaxe at bench"])
-		return state
-	
-	def op_wooden_pickaxe_for_coal(state, ID):
-		state = make_operator(data["Recipes"]["wooden_pickaxe for coal"])
-		return state
-	
-	def op_iron_pickaxe_for_ore(state, ID):
-		state = make_operator(data["Recipes"]["iron_pickaxe for ore"])
-		return state
-	
-	def op_wooden_axe_for_wood(state, ID):
-		state = make_operator(data["Recipes"]["wooden_axe for wood"])
-		return state
-	
-	def op_craft_plank(state, ID):
-		state = make_operator(data["Recipes"]["craft plank"])
-		return state
-	
-	def op_craft_stick(state, ID):
-		state = make_operator(data["Recipes"]["craft stick"])
-		return state
-	
-	def op_craft_rail_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft rail at bench"])
-		return state
-	
-	def op_craft_cart_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft cart at bench"])
-		return state
-	
-	def op_iron_pickaxe_for_cobble(state, ID):
-		state = make_operator(data["Recipes"]["iron_pickaxe for cobble"])
-		return state
-	
-	def op_stone_axe_for_wood(state, ID):
-		state = make_operator(data["Recipes"]["stone_axe for wood"])
-		return state
-	
-	def op_craft_iron_pickaxe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft iron_pickaxe at bench"])
-		return state
-	
-	def op_craft_furnace_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft furnace at bench"])
-		return state
-	
-	def op_stone_pickaxe_for_ore(state, ID):
-		state = make_operator(data["Recipes"]["stone_pickaxe for ore"])
-		return state
-	
-	def op_craft_iron_axe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft iron_axe at bench"])
-		return state
-	
-	def op_stone_pickaxe_for_coal(state, ID):
-		state = make_operator(data["Recipes"]["stone_pickaxe for coal"])
-		return state
-	
-	def op_stone_pickaxe_for_cobble(state, ID):
-		state = make_operator(data["Recipes"]["stone_pickaxe for cobble"])
-		return state
-	
-	def op_wooden_pickaxe_for_cobble(state, ID):
-		state = make_operator(data["Recipes"]["wooden_pickaxe for cobble"])
-		return state
-	
-	def op_iron_pickaxe_for_coal(state, ID):
-		state = make_operator(data["Recipes"]["iron_pickaxe for coal"])
-		return state
-	
-	def op_craft_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft bench"])
-		return state
-	
-	def op_craft_stone_axe_at_bench(state, ID):
-		state = make_operator(data["Recipes"]["craft stone_axe at bench"])
-		return state
-	
-	def op_smelt_ore_in_furnace(state, ID):
-		state = make_operator(data["Recipes"]["smelt ore in furnace"])
-		return state
-
-	pyhop.declare_operators (op_punch_for_wood, op_craft_wooden_axe_at_bench, op_iron_axe_for_wood, op_craft_wooden_pickaxe_at_bench, 
-						 op_craft_stone_pickaxe_at_bench, op_wooden_pickaxe_for_coal, op_iron_pickaxe_for_ore, op_wooden_axe_for_wood, 
-						 op_craft_plank, op_craft_stick, op_craft_rail_at_bench, op_craft_cart_at_bench, op_iron_pickaxe_for_cobble,
-						 op_stone_axe_for_wood, op_craft_iron_pickaxe_at_bench, op_craft_furnace_at_bench, op_stone_pickaxe_for_ore, 
-						 op_craft_iron_axe_at_bench, op_stone_pickaxe_for_coal, op_stone_pickaxe_for_cobble, op_wooden_pickaxe_for_cobble, 
-						 op_iron_pickaxe_for_coal, op_craft_bench, op_craft_stone_axe_at_bench, op_smelt_ore_in_furnace)
-	
-	pass
-
-def add_heuristic (data, ID):
-	# prune search branch if heuristic() returns True
-	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
-	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
-	def heuristic (state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
-
-		# use the data to get the recipes and the tools
-		# use the ID to get the agent's ID
-
-		# state = current state
-		# curr_task = current task
-		# use tasks to get the tasks that are left
-		# plan = current plan
-		# use the depth to get the current depth of the search tree
-		# calling_stack is to get the current task and the tasks that are left
-
-		# if the current task is complete, return true
-		# if the current task is not complete, check if the task can be completed
-		# if the task can be completed, return true
-		# else return false
-
-		for task in tasks:
-			if task[0] == curr_task:
-				return False
-			if curr_task == 'produce_furnace':
-				if getattr(state,'furnace')[ID] >= 1: return True
-			if curr_task == 'produce_bench':
-				if getattr(state,'bench')[ID] >= 1: return True
-			if curr_task == 'produce_iron_pickaxe':
-				if getattr(state,'iron_pickaxe')[ID] >= 1: return True
-			if curr_task == 'produce_iron_axe':
-				if getattr(state,'iron_axe')[ID] >= 1: return True
-			if curr_task == 'produce_stone_pickaxe':
-				if getattr(state,'stone_pickaxe')[ID] >= 1: return True
-			if curr_task == 'produce_stone_axe':
-				if getattr(state,'stone_axe')[ID] >= 1: return True
-			if curr_task == 'produce_wooden_pickaxe':
-				if getattr(state,'wooden_pickaxe')[ID] >= 1: return True
-			if curr_task == 'produce_wooden_axe':
-				if getattr(state,'wooden_axe')[ID] >= 1: return True
-			if curr_task == 'produce_cart':
-				if getattr(state,'cart')[ID] >= 1: return True
-			if curr_task == 'produce_rail':
-				if getattr(state,'rail')[ID] >= 20: return True
-
-		return False
-
-	pyhop.add_check(heuristic)
-
-def set_up_state (data, ID, time=0):
-	state = pyhop.State('state')
-	state.time = {ID: time}
-
-	for item in data['Items']:
-		setattr(state, item, {ID: 0})
-
-	for item in data['Tools']:
-		setattr(state, item, {ID: 0})
-
-	for item, num in data['Initial'].items():
-		setattr(state, item, {ID: num})
-
-	return state
-
-def set_up_goals (data, ID):
-	goals = []
-	for item, num in data['Goal'].items():
-		goals.append(('have_enough', ID, item, num))
-
-	return goals
-
-if __name__ == '__main__':
-	rules_filename = 'crafting.json'
-
-	with open(rules_filename) as f:
-		data = json.load(f)
-
-	state = set_up_state(data, 'agent', time=239) # allot time here
-	goals = set_up_goals(data, 'agent')
-
-	declare_operators(data)
-	declare_methods(data)
-	add_heuristic(data, 'agent')
-
-	# pyhop.print_operators()
-	# pyhop.print_methods()
-
-	# Hint: verbose output can take a long time even if the solution is correct; 
-	# try verbose=1 if it is taking too long
-	pyhop.pyhop(state, goals, verbose=3)
-	# pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+	pyhop.declare_methods ('produce_plank', craft_plank)	"""
